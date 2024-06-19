557d3748e43db5d42eae4a0f91ae53acf1443ad8
underwork <lenamquoc1@gmail.com>
2024-06-19 07:08:44 +0700

    Update DateTimeCheckerAppPython.UnitTests.py
    Added skip, expected failure, error catching and suite/runner demo

--- DateTimeCheckerAppPython.UnitTests/DateTimeCheckerAppPython.UnitTests.py
+++ DateTimeCheckerAppPython.UnitTests/DateTimeCheckerAppPython.UnitTests.py
@@ -1,17 +1,14 @@
 import unittest
 # Add references to the main project first to use this
 from DateTimeCheckerAppPython import DayInMonth, ValidDate
-
-class SharedSetupTeardown(unittest.TestCase):
+                        
+class TestValidDate(unittest.TestCase):
     def setUp(self):
-        self.day_in_month = DayInMonth()
         self.valid_date = ValidDate()
 
     def tearDown(self):
-        # Unnecessary in the given classes 
-        pass
-                        
-class TestValidDate(SharedSetupTeardown):
+        del self.valid_date
+    
     def test_is_valid_date(self):
         result = self.valid_date.check_date(30, 3, 1914)
         self.assertTrue(result)
@@ -32,7 +29,13 @@ class TestValidDate(SharedSetupTeardown):
         result = self.valid_date.check_date("", 3, 2025)
         self.assertFalse(result) 
         
-class TestDayInMonth(SharedSetupTeardown):
+class TestDayInMonth(unittest.TestCase):
+    def setUp(self):
+        self.day_in_month = DayInMonth()
+
+    def tearDown(self):
+        del self.day_in_month
+           
     def test_check_day_in_month_1(self):
         result = self.day_in_month.check_day_in_month(3, 2024)
         self.assertEqual(result, 31)
@@ -49,17 +52,29 @@ class TestDayInMonth(SharedSetupTeardown):
         result = self.day_in_month.check_day_in_month(2, 2020)
         self.assertEqual(result, 29)
         
-    def test_check_invalid_format(self):
+    def test_check_invalid_format(self): # Expected to Error
         result = self.day_in_month.check_day_in_month("xyz", 2025)
         self.assertEqual(result, -1)
-    
-    def test_check_empty_input(self):
+        # with self.assertRaises(ValueError):
+        #     result = self.day_in_month.check_day_in_month("xyz", 2025)
+               
+    # @unittest.skip("Demonstrating skipping a test")
+    def test_check_empty_input(self): # Expected to Error
         result = self.day_in_month.check_day_in_month("", 2025)
         self.assertEqual(result, -1)
-        
-    def test_check_out_of_boundary(self):
+       
+    # @unittest.expectedFailure    
+    def test_check_out_of_boundary(self): # Expected to fail
         result = self.day_in_month.check_day_in_month(13, 2025)
         self.assertEqual(result, 1) # Should return -1
+        
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(TestDayInMonth('test_check_non_leap_year'))
+    suite.addTest(TestDayInMonth('test_check_leap_year'))
+    return suite
 
 if __name__ == '__main__':
     unittest.main()
+    # runner = unittest.TextTestRunner()
+    # runner.run(suite())
